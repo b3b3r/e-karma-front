485ee5a43151c127a95463e7ef306d3df76a6bab
Spliit <mcuville@gmail.com>
2019-06-28 01:57:28 +0200

    Oui3

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
 
-import './Navbar.scss';
+import "./Navbar.scss";
 class Navbar extends Component {
-
   render() {
     const { pseudo, avatar } = this.props;
-    console.log({pseudo})
+    console.log({ pseudo });
     return (
       <div className="Navbar">
-        <img src ='/images/Logo.png' className='logo' alt="logo"/>
-        <NavLink className='avatar' to='/profil'>
-          <img src={avatar}  alt="profil" />
+        <NavLink to="/">
+          <img src="/images/Logo.png" className="logo" alt="logo" />
+        </NavLink>
+        <NavLink to="/profil">
+          <img src={avatar} className="avatar"  alt="profil" />
         </NavLink>
       </div>
     );
@@ -21,7 +22,7 @@ class Navbar extends Component {
 
 const mstp = state => ({
   pseudo: state.displayUser.pseudo,
-  avatar: state.displayUser.avatar,
+  avatar: state.displayUser.avatar
 });
 
 export default connect(mstp)(Navbar);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,7 @@
 const initialState = {
     id: 1,
     pseudo: 'babouche',
-    avatar: 'https://i.pinimg.com/474x/06/cf/27/06cf271240d72b7f9e6d5c9e1c04c0ed--dora-the-explorer-nick-jr.jpg',
+    avatar: '/images/avatars/avatar1.png',
 };
 
 const displayUser = (state = initialState, action) => {
