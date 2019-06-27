ac0a379ec6b6742f27bc88c7d453293d6987c70b
b3b3r <b3b3rseg@gmail.com>
2019-06-28 00:29:20 +0200

    creation composant badges

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Gamification from './containers/gamification'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Gamification />
     </div>
   );
 }
