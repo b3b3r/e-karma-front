88addc82ed61799aef1a6d50d560ba1ba467edae
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2019-06-28 02:22:49 +0200

    topic - start

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Topic from './containers/Topic';
 import Components from './Components';
 
 import './App.scss';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/topics/:id" component={Topic} />
         <Route path="/components" component={Components} />
       </Switch>
     </div>
