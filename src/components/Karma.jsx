db11224b7ebe068944b9ce748e702cfb07ce5b98
b3b3r <b3b3rseg@gmail.com>
2019-06-28 03:18:19 +0200

    merge dev

---
+++ src/components/Karma.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Karma() {
+  return (
+    <div className="Karma">
+      <p>Karma</p>
+      <canvas id="myChart"></canvas>
+    </div>
+  )
+}
+export default Karma;
