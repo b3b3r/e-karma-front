c30e8701421f7bd94e782ec9593561bede25afb2
b3b3r <b3b3rseg@gmail.com>
2019-06-28 12:23:13 +0200

    conflits constant

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export const urlApi = process.env.REACT_APP_API_SERVER;
 export const temp = 'temp';
-=======
-export const urlApi = process.env.REACT_APP_API_SERVER;
-export const temp = 'temp';
->>>>>>> dev
+
