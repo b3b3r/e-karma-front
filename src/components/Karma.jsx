f613eaac3804c645b794075e06832fe1980e38b6
b3b3r <b3b3rseg@gmail.com>
2019-06-28 04:23:03 +0200

    ajout chart

--- src/components/Karma.jsx
+++ src/components/Karma.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+const Chart = require("chart.js");
 
-function Karma() {
-  return (
-    <div className="Karma">
-      <p>Karma</p>
-<<<<<<< HEAD
-      <canvas id="myChart"></canvas>
-=======
->>>>>>> dev
-    </div>
-  )
+class Karma extends Component {
+  componentDidMount() {
+    const node = this.node;
+
+    const myChart = new Chart(node, {
+      type: "line",
+      data: {
+        labels: ["Trolls", "Bons messages", "Balance"],
+        datasets: [
+          {
+            label: "KARMA",
+            data: [12, 19, 7],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)"
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas
+          style={{ width: 400, height: 150 }}
+          ref={node => (this.node = node)}
+        />
+      </div>
+    );
+  }
 }
+
 export default Karma;
