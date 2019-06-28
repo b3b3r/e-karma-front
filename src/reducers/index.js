362576b97592f3161d5b0cd2fdac61ea3d4db470
Spliit <mcuville@gmail.com>
2019-06-28 11:48:17 +0200

    oui8

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import users from './users.js';
+import user from './user.js';
 import topics from './topics.js';
 
 const allReducers = combineReducers({
     users,
+    user,
     topics,
   });
   
