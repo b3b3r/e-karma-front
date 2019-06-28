2146cb7646b2e6c536d49b584c47e24d7d00bad1
b3b3r <b3b3rseg@gmail.com>
2019-06-28 08:04:54 +0200

    ajout# dans tags

--- src/reducers/topics.js
+++ src/reducers/topics.js
@@ -2,25 +2,25 @@ const initialState = [
     {
         id: 1,
         titre: 'Pourquoi mon chat est moche ?',
-        tags: ['animaux', 'questions con'],
+        tags: ['#animaux', '#questions con'],
         commentaires: 25,
     },
     {
         id: 2,
         titre: 'Pourquoi mon chien est moche ?',
-        tags: ['animaux', 'questions con'],
+        tags: ['#animaux', '#questions con'],
         commentaires: 2,
     },
     {
         id: 3,
         titre: 'Pourquoi LoL est mort ?',
-        tags: ['jeux vidéos', 'deadgames'],
+        tags: ['#jeux vidéos', '#deadgames'],
         commentaires: 16,
     },
     {
         id: 4,
         titre: 'Pourquoi Fortnite est bien ?',
-        tags: ['jeux vidéos', 'Battle Royal'],
+        tags: ['#jeux vidéos', '#Battle Royal'],
         commentaires: 52,
     }
 ]
