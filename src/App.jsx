19ce8441c8a7a118e8339708cf880a7a2e29d8e4
Spliit <mcuville@gmail.com>
2019-06-28 07:13:21 +0200

    oui6

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from './containers/Navbar';
 import Profil from './components/Profil';
 import Home from './containers/Home';
-import Profil from './components/Profil';
 import Historique from './components/Historique';
 import Gamification from './components/Gamification';
 import Karma from './components/Karma';
-import { Switch, Route } from 'react-router-dom';
 
-import Navbar from './containers/Navbar';
 import Topic from './containers/Topic';
 import Components from './Components';
 
